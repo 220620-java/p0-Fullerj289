7f1d91baa03c8b68749068c1753fef6ab8f5e73b
Fuller <fullerj@wwt.com>
2022-06-23 23:36:59 -0500

    update to user

--- UserInfo.java
+++ UserInfo.java
@@ -1,5 +1,18 @@
 package com.revature.james;
 
 public class UserInfo {
-
+	String firstName;
+	String lastName;
+	String email;
+	int age;
+	
+	
+	
+	public UserInfo() {
+		this.firstName = "firstName";
+		this.lastName = "lastName";
+		this.age = 0;
+		
+		
+	}
 }
