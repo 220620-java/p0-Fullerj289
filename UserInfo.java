8099c79ddce3db82a5c30508b7b706d3547d3da2
Fuller <fullerj@wwt.com>
2022-06-25 12:44:51 -0500

    working progess 2

--- UserInfo.java
+++ UserInfo.java
@@ -1,18 +1,36 @@
 package com.revature.james;
 
+import java.util.Scanner;
+
 public class UserInfo {
-	String firstName;
-	String lastName;
-	String email;
-	int age;
+	private String firstName = "firstName";
+	private String lastName = "lastName";
+	private String fullName = firstName + lastName;
+	private String email;
+	private int age;
 	
 	
 	
-	public UserInfo() {
+	public void setUserInfo(String name) {
 		this.firstName = "firstName";
 		this.lastName = "lastName";
 		this.age = 0;
 		
 		
 	}
+	public String getUserInfo() {
+		
+		
+	}
+	public static void userInput() {
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Enter Full Name: ");
+		String fullName = userInput.next();
+		System.out.println("Welcome " + fullName);
+		
+		
+		
+		
+	}
+	
 }
