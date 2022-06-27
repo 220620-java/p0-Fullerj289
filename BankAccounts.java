266122f0c3acee86996274f782ec635f6c92d649
Fuller <fullerj@wwt.com>
2022-06-26 23:02:23 -0500

    updated the userclass

--- BankAccounts.java
+++ BankAccounts.java
@@ -1,5 +1,21 @@
 package com.revature.james;
 
 public class BankAccounts {
+	// has-A relation with bank
+	
+	
+	
+	public static double deposit() {
+		//user can put their money in
+		
+	}
+	public static double withdraw() {
+		//user can take their money out
+		
+	}
+	public static double inquire() {
+		//user can see what's in their account
+		
+	}
 
 }
--- BankApp.java
+++ BankApp.java
@@ -1,10 +1,18 @@
 package com.revature.james;
 
+
+// first I analysis and design concept from the OOAD(object-oriented analysis and design)
 public class BankApp {
+	//defining what the system is supposed to do
+	// specifying how the system should do it
+	
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
 
 }
